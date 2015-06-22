e5daaca47842af87bd325f00c7e306d0ffb6a35a
take5249 <take5249@hotmail.com>
2015-06-23 00:19:47 +0900

    add license text

--- app/src/main/java/jp/wasshoi/take5249/MazeApp/MainActivity.java
+++ app/src/main/java/jp/wasshoi/take5249/MazeApp/MainActivity.java
@@ -11,6 +11,21 @@ import jp.wasshoi.take5249.MazeApp.MazeCore.Maze;
 import jp.wasshoi.take5249.MazeApp.MazeCore.Player;
 import jp.wasshoi.take5249.MazeApp.R;
 
+/**
+ Copyright 2015 take5249
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+ http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+ */
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
 	protected FirstPersonView fpView;
